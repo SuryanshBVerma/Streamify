ab7ba6a71938a19722ee8176586e18dca76d0a80
“SuryanshBVerma-christuniversity” <suryansh.bachchanverma@mca.christuniversity.in>
2024-08-26 13:50:56 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+
+  return (
+    <>
+      <main className="m-4">
+
+      <Button>Click me</Button>
+      </main>
+
+    </>
+  )
+}
+
+export default App
