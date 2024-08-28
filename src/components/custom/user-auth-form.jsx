fd049a58abd9e156dfb6f822904d25e86d14993e
“SuryanshBVerma-christuniversity” <suryansh.bachchanverma@mca.christuniversity.in>
2024-08-28 23:44:25 +0530

    Updeted for deployment

--- src/components/custom/user-auth-form.jsx
+++ src/components/custom/user-auth-form.jsx
@@ -9,7 +9,7 @@ import {
     FormMessage
 } from '@/components/ui/form';
 
-import { Input } from '@/components/ui/Input'; // Adjust the path to your input component
+import { Input } from '@/components/ui/input'; // Adjust the path to your input component
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
